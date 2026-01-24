2a708dc417b1af3b4b3ba298dc4d532269b4fb76
Austin Evans <austinjevans@me.com>
2026-01-24 18:11:26 -0500

    Remove Playground executable target from Package.swift

--- Sources/Playground/main.swift
+++
@@ -1,9 +0,0 @@
-import PodcastTransferCore
-
-@main
-enum PlaygroundMain {
-  static func main() async throws {
-    let episodes = try await PodcastLibraryClient.preview.loadEpisodes()
-    print("Preview episodes available: \(episodes.count)")
-  }
-}
